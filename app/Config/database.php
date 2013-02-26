950df5f338799b79a0454c98516c499372016f05
jusdoranges <philippe.gremmel@gmail.com>
2013-02-26 09:45:05 +0100

    added config

---
+++ app/Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'port' => 8889,
+		'login' => 'cakephp',
+		'password' => 'cakephp',
+		'database' => 'cake_eatme',
+		'encoding' => 'UTF8'
+	);
+}
