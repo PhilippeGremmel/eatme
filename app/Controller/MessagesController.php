eaa5fa69840855ce2aa7d0fc06caaf18c0daf5ab
Awad Patricia <awadpatricia@hotmail.com>
2013-02-27 11:39:18 +0100

    27/02/2013_2_whatdidido
    Home deleted + modifications : MessagesController.php, Message.php, &
    add.ctp of Messages. Messages system created.

--- app/Controller/MessagesController.php
+++ app/Controller/MessagesController.php
@@ -7,6 +7,24 @@ App::uses('AppController', 'Controller');
  */
 class MessagesController extends AppController {
 
+	public function isAuthorized($user) {
+		switch ($this->action) {
+			case 'add':
+				return isset($user['id']); // utilisateurs connectés
+
+			case 'delete':
+			case 'edit':
+				if(isset($user['group_id']) && $user['group_id'] == 1) // admin
+					return true;
+				
+				$Message_id = $this->request->params['pass'][0]; // auteur
+				$user_id = $user['id'];
+				if($this->Message->isOwnedBy($message_id, $user_id))
+					return true;
+		}
+		return parent::isAuthorized($user);
+	}
+
 /**
  * index method
  *
@@ -26,7 +44,7 @@ class MessagesController extends AppController {
  */
 	public function view($id = null) {
 		if (!$this->Message->exists($id)) {
-			throw new NotFoundException(__('Invalid message'));
+			throw new NotFoundException(__('Invalid post'));
 		}
 		$options = array('conditions' => array('Message.' . $this->Message->primaryKey => $id));
 		$this->set('message', $this->Message->find('first', $options));
@@ -40,15 +58,17 @@ class MessagesController extends AppController {
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Message->create();
+			$this->request->data['Message']['user_id'] = $this->viewVars['me']['id'];
 			if ($this->Message->save($this->request->data)) {
-				$this->Session->setFlash(__('The message has been saved'));
+				$this->Session->setFlash(__('The message has been sent'));
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The message could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The message could not be sent. Please, try again.'));
 			}
 		}
-		$users = $this->Message->User->find('list');
-		$this->set(compact('users'));
+		$dests = $this->Message->User->find('list');
+		$this->set(compact('dests'));
+		$this->set('message','yop');
 	}
 
 /**
@@ -60,14 +80,14 @@ class MessagesController extends AppController {
  */
 	public function edit($id = null) {
 		if (!$this->Message->exists($id)) {
-			throw new NotFoundException(__('Invalid message'));
+			throw new NotFoundException(__('Invalid post'));
 		}
 		if ($this->request->is('post') || $this->request->is('put')) {
 			if ($this->Message->save($this->request->data)) {
-				$this->Session->setFlash(__('The message has been saved'));
+				$this->Session->setFlash(__('The message has been sent'));
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The message could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The message could not be sent. Please, try again.'));
 			}
 		} else {
 			$options = array('conditions' => array('Message.' . $this->Message->primaryKey => $id));
@@ -88,14 +108,14 @@ class MessagesController extends AppController {
 	public function delete($id = null) {
 		$this->Message->id = $id;
 		if (!$this->Message->exists()) {
-			throw new NotFoundException(__('Invalid message'));
+			throw new NotFoundException(__('Invalid post'));
 		}
 		$this->request->onlyAllow('post', 'delete');
 		if ($this->Message->delete()) {
-			$this->Session->setFlash(__('Message deleted'));
+			$this->Session->setFlash(__('Post removed'));
 			$this->redirect(array('action' => 'index'));
 		}
-		$this->Session->setFlash(__('Message was not deleted'));
+		$this->Session->setFlash(__('The post was not deleted'));
 		$this->redirect(array('action' => 'index'));
 	}
 
@@ -118,7 +138,7 @@ class MessagesController extends AppController {
  */
 	public function admin_view($id = null) {
 		if (!$this->Message->exists($id)) {
-			throw new NotFoundException(__('Invalid message'));
+			throw new NotFoundException(__('Invalid post'));
 		}
 		$options = array('conditions' => array('Message.' . $this->Message->primaryKey => $id));
 		$this->set('message', $this->Message->find('first', $options));
@@ -133,10 +153,10 @@ class MessagesController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Message->create();
 			if ($this->Message->save($this->request->data)) {
-				$this->Session->setFlash(__('The message has been saved'));
+				$this->Session->setFlash(__('The message has been sent'));
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The message could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The message could not be sent. Please, try again.'));
 			}
 		}
 		$users = $this->Message->User->find('list');
@@ -152,14 +172,14 @@ class MessagesController extends AppController {
  */
 	public function admin_edit($id = null) {
 		if (!$this->Message->exists($id)) {
-			throw new NotFoundException(__('Invalid message'));
+			throw new NotFoundException(__('Invalid post'));
 		}
 		if ($this->request->is('post') || $this->request->is('put')) {
 			if ($this->Message->save($this->request->data)) {
-				$this->Session->setFlash(__('The message has been saved'));
+				$this->Session->setFlash(__('The post has been saved'));
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The message could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The post could not be saved. Please, try again.'));
 			}
 		} else {
 			$options = array('conditions' => array('Message.' . $this->Message->primaryKey => $id));
@@ -180,14 +200,14 @@ class MessagesController extends AppController {
 	public function admin_delete($id = null) {
 		$this->Message->id = $id;
 		if (!$this->Message->exists()) {
-			throw new NotFoundException(__('Invalid message'));
+			throw new NotFoundException(__('Invalid post'));
 		}
 		$this->request->onlyAllow('post', 'delete');
 		if ($this->Message->delete()) {
-			$this->Session->setFlash(__('Message deleted'));
+			$this->Session->setFlash(__('post deleted'));
 			$this->redirect(array('action' => 'index'));
 		}
-		$this->Session->setFlash(__('Message was not deleted'));
+		$this->Session->setFlash(__('post was not deleted'));
 		$this->redirect(array('action' => 'index'));
 	}
 }
