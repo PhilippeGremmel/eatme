fc968557323b5b086cd7c5d1290d1f73da1b7b6d
sea-reel <cyril.garsaud@gmail.com>
2013-03-01 13:02:53 +0100

    Bugs

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,14 +1,22 @@
 <?php
 class DATABASE_CONFIG {
-
-	private $cyril = false;
-
+/*
 	public $default = array(
 		'datasource' => 'Database/mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => ($cyril?'root':'cakephp'),
-		'password' => ($cyril?'root':'cakephp'),
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'cake_eatme',
+		'encoding' => 'UTF8'
+	);
+*/
+	public $default = array(
+		'datasource' => 'Database/mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'cakephp',
+		'password' => 'cakephp',
 		'database' => 'cake_eatme',
 		'encoding' => 'UTF8'
 	);
