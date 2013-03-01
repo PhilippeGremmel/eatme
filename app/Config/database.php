9cbcfa5f20553204da4860bd66db31dd47bee0ee
sea-reel <cyril.garsaud@gmail.com>
2013-03-01 13:00:56 +0100

    réglage bdd

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,12 +1,14 @@
 <?php
 class DATABASE_CONFIG {
 
+	private $cyril = false;
+
 	public $default = array(
 		'datasource' => 'Database/mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'cakephp',
-		'password' => 'cakephp',
+		'login' => ($cyril?'root':'cakephp'),
+		'password' => ($cyril?'root':'cakephp'),
 		'database' => 'cake_eatme',
 		'encoding' => 'UTF8'
 	);
